b0a1d48d385ca5cbf59a5ef1624630f025d06e10
liang <liang@liangdeMacBook-Pro.local>
2016-08-20 10:25:06 +0800

    大衣
    添加了一个打印的功能，可以让代码有输出的功能

---
+++ frfrfrfrfrfrfrfrfrfrfrfr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  frfrfrfrfrfrfrfrfrfrfrfr
+//
+//  Created by liang on 16/8/3.
+//  Copyright © 2016年 paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ frfrfrfrfrfrfrfrfrfrfrfr/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  frfrfrfrfrfrfrfrfrfrfrfr
+//
+//  Created by liang on 16/8/3.
+//  Copyright © 2016年 paul. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+     NSLog(@"hello world");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
